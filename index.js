048df1a5323b99812155b1aafea28a9a9dd6ac57
Jared S <jaredjames2020@gmail.com>
2020-04-24 16:12:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
 // function map(array, func){
 //   return array.map(num => num*-1);
 // } 
+
+
+
+function map(array, func) {
+  let newArray = [];
+  for (let value of array)
+    newArray.push(func(value));
+  }
+  return newArray;
+};
