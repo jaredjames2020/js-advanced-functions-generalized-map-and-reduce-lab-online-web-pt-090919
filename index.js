8994b3bfc96b9b2a212d059edf770bc9f759b1be
Jared S <jaredjames2020@gmail.com>
2020-04-24 15:51:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Add your functions here
 function map(array){
-  let whatever = [];
-  whatever.push(array * -1);
-  return whatever;
-} ;
+  // let whatever = [];
+  // whatever.push(array * -1);
+  // return whatever;
+  array.map(num*-1)
+} 
